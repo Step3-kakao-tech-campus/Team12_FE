7d8a61139506e44993022118c9b028d77ed09553
rktdnjs <rktdnjs@naver.com>
2023-09-25 00:15:26 +0900

    [conf] : Pretendard 폰트 전역 적용 & 기본 레이아웃
    - 현재 잡아놓은 레이아웃은 iframe을 이용한 레이아웃 x

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import './global.css';
+
 function App() {
   return (
-    <div className="App">
-      <p>App.js 입니다.</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="page--layout">
+      <h1>픽업셔틀</h1>
+    </div>
+  );
+};
+
+export default HomePage;
