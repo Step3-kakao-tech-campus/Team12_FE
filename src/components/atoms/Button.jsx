c570bea7af7e7ae032b710ccdddd29c78619bc2e
rktdnjs <rktdnjs@naver.com>
2023-09-28 18:35:05 +0900

    [feat] : Button.jsx 구현

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,26 @@
+// 필요에 맞게 너비, 높이, 배경색, 글자색, 테두리둥금 수치를 상위 컴포넌트에서 조정
+// props default 값은 로그인 버튼 기준입니다!
+const Button = ({
+  type,
+  onClick,
+  disabled,
+  children,
+  width = 'w-64',
+  height = 'h-8',
+  bgColor = 'bg-blue-600',
+  textColor = 'text-white',
+  bdRadius = 'rounded-lg',
+}) => {
+  return (
+    <button
+      type={type || 'button'}
+      onClick={onClick}
+      disabled={disabled}
+      className={`${width} ${height} ${textColor} ${bgColor} ${bdRadius}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
