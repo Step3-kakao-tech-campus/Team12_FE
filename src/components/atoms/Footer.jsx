b44ee7783004f7840e412424b1c78d8d8a05168d
rlaeksqls@naver.com <rlaeksqls@naver.com>
2023-09-30 16:18:02 +0900

    [feat]:Footer 구현

---
+++ src/components/atoms/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="bg-zinc-100 p-5 w-full absolute bottom-0">
+      <span className="text-zinc-400">픽업셔틀</span>
+      <br />
+      <span className="text-zinc-400 text-xs">카테캠 전남대 12조</span>
+      <hr className="border-1 border-zinc-300" />
+      <div className="text-right m-1 text-zinc-400">Copyright &#169;</div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer from '../components/atoms/Footer';
 
 const HomePage = () => {
   return (
     <div className="page--layout">
       <h1>픽업셔틀</h1>
+      <Footer />
     </div>
   );
 };
