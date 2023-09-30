d4ef11a9a4999bacdeff4d2dc6af4a2d6ba1e0c9
rktdnjs <rktdnjs@naver.com>
2023-09-30 19:57:03 +0900

    [feat] : 카카오 소셜 로그인 페이지 개발
    - 카카오 인가 코드를 받아서 처리하기까지의 큰 틀 구현
    - 로그인 이후에 계좌정보 유무에 따른 추가 정보 기입 페이지 구현 필요
    - 현재는 임시로 토큰을 만들어, 바로 로그인 처리 및 토큰 저장이 되도록 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import KakaoOuathPage from './pages/KakaoOuathPage';
 import './global.css';
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login/kakao" element={<KakaoOuathPage />} />
       </Routes>
     </BrowserRouter>
   );
