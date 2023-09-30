ee29d6d145df925341557402e389c54468bbe8d4
baegyeong <beth0929@naver.com>
2023-09-30 18:44:34 +0900

    [feat]: 공고 작성 페이지 라우터 지정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import StartPostPage from './pages/StartPostPage';
+import PostWritePage from './pages/PostWritePage';
 import './global.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/start-post" element={<StartPostPage />} />
+        <Route path="/post-write" element={<PostWritePage />} />
       </Routes>
     </BrowserRouter>
   );
