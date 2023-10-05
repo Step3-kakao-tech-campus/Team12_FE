32f5f66b6240e5af520efe0b6dbac797ca89c89d
baegyeong <beth0929@naver.com>
2023-10-06 01:58:06 +0900

    [fix]: 버튼 컴포넌트로 변경
    - 버튼 컴포넌트 추가
    - Link로 navigate 지정

--- src/pages/StartPostPage.jsx
+++ src/pages/StartPostPage.jsx
@@ -1,12 +1,20 @@
+import { Link } from 'react-router-dom';
+import Button from '../components/atoms/Button';
+
 const StartPostPage = () => {
   return (
-    <div className="page--layout bg-sky-blue flex flex-col items-start justify-end">
+    <div className="page--layout bg-sky-blue flex flex-col justify-end">
       <div className="text-white p-8">
         <div className="font-bold text-xl">어떤 음료를 픽업 받으실 건가요?</div>
         <div>픽업을 위한 정보를 입력합니다.</div>
       </div>
-      {/* 버튼 컴포넌트로 수정 */}
-      <button type="button">공고 작성하기</button>
+      <div className="flex justify-center mb-14">
+        <Link to="/post-write">
+          <Button height="w-[19rem] h-[2.5rem]" bgColor="bg-white" textColor="text-sky-blue">
+            공고 작성하기
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
