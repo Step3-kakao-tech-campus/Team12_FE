8abc77d0d3d139e9ddd5da9811f71f3286f62e51
baegyeong <beth0929@naver.com>
2023-10-07 16:15:07 +0900

    [fix]: 현재 위치에 따라 색상 다르게 지정

--- src/components/molecules/CircleBtn.jsx
+++ src/components/molecules/CircleBtn.jsx
@@ -1,12 +1,15 @@
 import Button from '../atoms/Button';
 
-const CircleBtn = ({ num, children }) => {
+const CircleBtn = ({ num, children, active }) => {
+  const bgColor = active ? 'bg-blue' : 'bg-gray-400';
+  const textColor = active ? 'text-blue' : 'text-gray-400';
+
   return (
     <div className="flex flex-col items-center z-10">
-      <Button width="w-8" bgColor="bg-blue" bdRadius="rounded-full">
+      <Button bgColor={bgColor} width="w-8" bdRadius="rounded-full">
         {num}
       </Button>
-      <div className="mt-2 text-blue">{children}</div>
+      <div className={`mt-2 ${textColor}`}>{children}</div>
     </div>
   );
 };
