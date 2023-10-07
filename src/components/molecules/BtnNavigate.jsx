0649a9b13018bb9d9532f415a609e3414e889471
baegyeong <beth0929@naver.com>
2023-10-07 16:15:51 +0900

    [fix]: onClick 함수를 prop으로 변경

--- src/components/molecules/BtnNavigate.jsx
+++ src/components/molecules/BtnNavigate.jsx
@@ -1,16 +1,22 @@
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import Button from '../atoms/Button';
 
-const BtnNavigate = () => {
+const BtnNavigate = ({ handlePrev, handleNext }) => {
   return (
     <div className="flex place-content-around m-6">
-      <Button width="w-[8rem]" bgColor="bg-white" textColor="text-blue" bdRadius="rounded-lg border-blue border-2">
+      <Button
+        onClick={handlePrev}
+        width="w-[8rem]"
+        bgColor="bg-white"
+        textColor="text-blue"
+        bdRadius="rounded-lg border-blue border-2"
+      >
         <div className="flex items-center">
           <IoIosArrowBack size={20} className="text-left mr-9" />
           이전
         </div>
       </Button>
-      <Button width="w-[8rem]" bgColor="bg-blue">
+      <Button onClick={handleNext} width="w-[8rem]" bgColor="bg-blue">
         <div className="flex items-center justify-end">
           다음
           <IoIosArrowForward size={20} className="ml-9" />
