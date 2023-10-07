699ebaa7d9d30b6068869b0da087aa8c9ba6f3b1
baegyeong <beth0929@naver.com>
2023-10-08 00:56:31 +0900

    [feat]: 여러 줄 입력을 위한 TextArea 컴포넌트 구현

---
+++ src/components/atoms/TextArea.jsx
@@ -0,0 +1,14 @@
+const TextArea = ({ placeholder, id, ...inputProps }) => {
+  return (
+    <textarea
+      type="text"
+      rows="3"
+      id={id}
+      className="w-[18rem] rounded-lg border-gray-300 border-2 px-5 py-2 text-sm block my-4"
+      placeholder={placeholder}
+      {...inputProps}
+    />
+  );
+};
+
+export default TextArea;
