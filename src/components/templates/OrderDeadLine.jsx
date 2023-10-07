34644223aee25c51c6924a2cb7af9c7a1dc4e17f
baegyeong <beth0929@naver.com>
2023-10-08 00:57:41 +0900

    [fix]: 숫자값 max, min 값 설정

--- src/components/templates/OrderDeadLine.jsx
+++ src/components/templates/OrderDeadLine.jsx
@@ -3,15 +3,18 @@ import Input from '../atoms/Input';
 
 const OrderDeadLine = () => {
   return (
-    <>
+    <div className="mt-6 mb-12">
       <Labels
         htmlFor="deadline"
         label="공고를 언제까지 띄울까요? *"
         subLabel="마감기한을 설정해주세요! 마감기한이 지나면 공고가 사라져요."
       />
-      <Input id="deadline" placeholder="13" />
-      <Input id="deadline-1" placeholder="15" />
-    </>
+      <div className="flex items-center place-content-between">
+        <Input type="number" min="0" max="24" id="deadline" width="w-[6rem]" placeholder="13" />시
+        <Input type="number" min="0" max="24" id="deadline-1" width="w-[6rem]" placeholder="15" />
+        분까지
+      </div>
+    </div>
   );
 };
 
