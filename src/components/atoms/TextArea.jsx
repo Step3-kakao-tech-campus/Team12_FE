6c8d66d32c03d1673a2a9d6164ad8a3457140b91
baegyeong <beth0929@naver.com>
2023-10-13 02:11:15 +0900

    [fix]: padding 값 변경

--- src/components/atoms/TextArea.jsx
+++ src/components/atoms/TextArea.jsx
@@ -4,7 +4,7 @@ const TextArea = ({ placeholder, id, ...inputProps }) => {
       type="text"
       rows="3"
       id={id}
-      className="w-[18rem] rounded-lg border-gray-300 border-2 px-5 py-2 text-sm block my-4"
+      className="w-[18rem] rounded-lg border-gray-300 border-2 px-4 py-2 text-sm block my-4"
       placeholder={placeholder}
       {...inputProps}
     />
