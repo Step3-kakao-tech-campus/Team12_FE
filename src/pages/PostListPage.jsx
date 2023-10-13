0200ab4f4de24298dcb68327d0aadd12137c33c4
rktdnjs <rktdnjs@naver.com>
2023-10-13 13:36:28 +0900

    [feat] : 공고 현황 페이지 필터링 기능 구현
    - PostListPage - FilterForm - FilterBtn 사이의 데이터 흐름
    - 최상단 PostListPage에서 데이터 페칭
    - FilterForm에서 필터 버튼 클릭 - 필터값 PostListPage로 전달
    - PostListPage에서는 필터값을 바탕으로 페칭한 데이터 필터링 하여 렌더링 처리
    - 데이터페칭 코드 추가후에 컴포넌트간 데이터 흐름 디버깅 필요할 것으로 예상

--- src/pages/PostListPage.jsx
+++ src/pages/PostListPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import OtherNav from '../components/atoms/OtherNav';
 import Cards from '../components/molecules/Cards';
@@ -6,8 +6,20 @@ import FilterForm from '../components/molecules/FilterForm';
 import WritePostIcon from '../assets/images/postWrite.png';
 
 const PostListPage = () => {
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState('');
+  const navigate = useNavigate();
+
   // 임시 더미 데이터(데이터를 서버로부터 fetch 해왔다고 가정)
   const data = [
+    {
+      boardId: 9,
+      shopName: '전남대 후문 더벤티',
+      destination: '공과대학 7호관',
+      finishedAt: 1696992289,
+      tip: 1500,
+      match: true,
+    },
     {
       boardId: 8,
       shopName: '전남대 후문 스타벅스',
@@ -18,7 +30,7 @@ const PostListPage = () => {
     },
     {
       boardId: 7,
-      shopName: '전남대 후문 벤티',
+      shopName: '전남대 후문 더벤티',
       destination: '공과대학 6호관',
       finishedAt: 1696040640,
       tip: 1000,
@@ -34,7 +46,7 @@ const PostListPage = () => {
     },
     {
       boardId: 5,
-      shopName: '전남대 후문 스타벅스',
+      shopName: '전남대 후문 공차',
       destination: '공과대학 7호관',
       finishedAt: 1696992289,
       tip: 1000,
@@ -62,11 +74,11 @@ const PostListPage = () => {
       destination: '공과대학 7호관',
       finishedAt: 1696040640,
       tip: 1000,
-      match: true,
+      match: false,
     },
     {
       boardId: 1,
-      shopName: '전남대 후문 이디야',
+      shopName: '전남대 후문 공차',
       destination: '공과대학 7호관',
       finishedAt: 1696040640,
       tip: 1000,
@@ -74,19 +86,29 @@ const PostListPage = () => {
     },
   ];
 
-  const navigate = useNavigate();
-
   const goWritePost = () => {
     navigate('/post-write-intro');
   };
+
+  // FilterForm으로부터 사용자가 설정한 filter를 받아옴
+  const getFilter = (text) => {
+    setFilter(text);
+  };
+
+  // filter가 업데이트 될 경우, 받아왔던 데이터를 filter에 따라 렌더링
+  // 추후 데이터 페칭 코드가 추가되면 동기적으로 작동하도록 aysnc - await 사용 필요
+  // 데이터 페칭 - posts 업데이트 - 필터에 따라 렌더링
+  useEffect(() => {
+    setPosts(data.filter((post) => post.shopName.includes(filter)));
+  }, [filter]);
 
   return (
     <div className="page--layout">
       <OtherNav />
       <div className="text-center text-blue text-xl">공고 현황</div>
-      <FilterForm />
+      <FilterForm getFilter={getFilter} />
       <div className="h-[550px] overflow-y-auto overflow-x-hidden scrollbar-hide">
-        <Cards articles={data} />
+        <Cards articles={posts} />
       </div>
       <div className="flex flex-row-reverse h-[84px] items-center px-[22px]">
         <img className="cursor-pointer" onClick={goWritePost} src={WritePostIcon} alt="공고 쓰기" />
