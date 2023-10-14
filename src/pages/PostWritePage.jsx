0555d9be8455421a3d4918d4fcf87ba7f77bb523
baegyeong <beth0929@naver.com>
2023-10-14 21:56:13 +0900

    [feat]: 상위 페이지 컴포넌트에서 주문정보 데이터 관리
    - react-hook-form 라이브러리 사용
    - errors에 따라 handleNext를 다루려고 했으나 두번이상 클릭 시 에러가 생략되는
      문제 -> useWatch로 해결

--- src/pages/PostWritePage.jsx
+++ src/pages/PostWritePage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm, useWatch } from 'react-hook-form';
 import OtherNav from '../components/atoms/OtherNav';
 import BtnNavigate from '../components/molecules/BtnNavigate';
 import OrderInfo from '../components/templates/OrderInfo';
@@ -10,7 +11,26 @@ import CircleNavigate from '../components/organisms/CircleNavigate';
 const PostWritePage = () => {
   const navigate = useNavigate();
   const [focus, setFocus] = useState(1);
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      store: '',
+      beverage: '',
+      destination: '',
+      tip: '',
+      request: '',
+      finishedAt: '',
+    },
+  });
   let currentPage;
+
+  const orderInfo = useWatch({
+    control,
+    name: ['store', 'beverage'],
+  });
 
   const handlePrev = () => {
     if (focus > 1) {
@@ -22,13 +42,21 @@ const PostWritePage = () => {
 
   const handleNext = () => {
     if (focus < 3) {
-      setFocus((prev) => prev + 1);
+      if (focus === 1) {
+        if (orderInfo[0] && orderInfo[1]) {
+          setFocus((prev) => prev + 1);
+        }
+      }
     }
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   switch (focus) {
     case 1:
-      currentPage = <OrderInfo />;
+      currentPage = <OrderInfo control={control} storeError={!!errors.store} beverageError={!!errors.beverage} />;
       break;
     case 2:
       currentPage = <OrderRequest />;
@@ -41,16 +69,18 @@ const PostWritePage = () => {
   }
 
   return (
-    <div className="page--layout flex flex-col justify-between">
-      <div className="flex flex-col justify-start">
-        <OtherNav />
-        <CircleNavigate navigate={focus} />
-        <div className="p-[35px]">{currentPage}</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="page--layout flex flex-col justify-between">
+        <div className="flex flex-col justify-start">
+          <OtherNav />
+          <CircleNavigate navigate={focus} />
+          <div className="p-[35px]">{currentPage}</div>
+        </div>
+        <div className="mb-8">
+          <BtnNavigate handlePrev={handlePrev} handleNext={handleNext} />
+        </div>
       </div>
-      <div className="mb-8">
-        <BtnNavigate handlePrev={handlePrev} handleNext={handleNext} />
-      </div>
-    </div>
+    </form>
   );
 };
 
