aa0c37d50c7ef0bf0a725524023f69c750686351
rktdnjs <rktdnjs@naver.com>
2023-10-14 14:14:56 +0900

    [feat] : 공고 현황 페이지 무한스크롤 구현(API 연결 확인 x)
    - react-intersection-observer useInView 훅 & react-query useInfiniteQuery 훅 활용
    - 스크롤 감지 = useInView / 데이터 페칭 = useInfiniteQuery
    - useInView에서 제공하는 ref를 페이지 요소로 걸어두고 inView값이 노출되면 추가 데이터를 불러오는 구조
    - 초회에 offset을 아무것도 주지 않고 요청하여 받아오는 데이터에서 마지막 공고를 id값을 이용하여 pageOffset으로 이용
    - 만약 마지막으로 받아왔던 데이터에 last 값이 true일 경우, 더이상 데이터를 불러오지 않도록 params로 null을 리턴
    - eslint로 인해 오류로 잡는 부분이 많아 현재는 페이지가 돌아가도록 코드가 많은 상태
    - 백엔드 API연결 이후에 전체적으로 다시 리팩토링 진행 예정

--- src/pages/PostListPage.jsx
+++ src/pages/PostListPage.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useInView } from 'react-intersection-observer';
+import { useInfiniteQuery } from '@tanstack/react-query';
+// import { getPosts } from '../apis/post';
 import OtherNav from '../components/atoms/OtherNav';
 import Cards from '../components/molecules/Cards';
 import FilterForm from '../components/molecules/FilterForm';
 import WritePostIcon from '../assets/images/postWrite.png';
 
 const PostListPage = () => {
+  // 이 코드는 inView에 닿았을 시 무한스크롤에 따라 요청이 이루어지는지 확인하기 위한 코드(정상 작동 o)
+  // 현재 index.js의 REACT_APP_API_URL이 따로 설정되지 않아 에러가 발생하는듯 하여 이렇게 임시로 작동만되도록 놔둠
+  // 추후 백엔드 API 연결이 완료되고 나면 삭제할 코드이므로, 아래 코드는 무시하셔도 됩니다.
+  const getPosts = (offset = '') => {
+    console.log(`요청  : /articles?offset=${offset}&limit=10`);
+  };
+
   // 임시 더미 데이터(데이터를 서버로부터 fetch 해왔다고 가정)
-  const data = [
+  const sampleData = [
     {
       boardId: 9,
       shopName: '전남대 후문 더벤티',
@@ -83,19 +92,47 @@ const PostListPage = () => {
     },
   ];
 
+  const navigate = useNavigate();
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState('');
-  const navigate = useNavigate();
-
   const { ref, inView } = useInView({ threshold: 0.5 });
+  const { data, fetchNextPage, hasNextPage, isError } = useInfiniteQuery(
+    ['posts'],
+    ({ pageParam = '' }) => getPosts(pageParam),
+    {
+      getNextPageParam: (lastPage) => {
+        // 다음 공고글을 호출할 때 사용할 pageParam(pageOffset)
+        // 초회 요청시에는 API문서 내용에 따라 아무것도 넣지 않아야 하므로 ''를 넣고, 이후에는 boardId값 기반으로 요청
+        // 마지막 페이지가 아닐경우 추가 렌더링을 위해 offset 계산하여 리턴 or 마지막 페이지일 경우 NULL을 반환하여 페이지를 불러오지 않도록 함
+        // 추후 백엔드 API 연결이 완료되고 나면 삭제할 코드이므로, 아래 코드는 무시하셔도 됩니다.
+        console.log('다음 param 호출');
+        console.log('lastPage', lastPage);
+        return 10;
+        // 여기 아래의 코드가 API연결 이후에 사용할 진짜 코드!
+        // return !lastPage.last ? lastPage.content[lastPage.numberOfElements - 1].boardId : null;
+      },
+    },
+  );
 
-  // inView가 사용자에게 보이면 다음 페이지를 렌더링해온다.
+  // inView가 사용자에게 보임 & hasNextPage가 true일 경우 다음 페이지를 렌더링해오도록 한다.
   useEffect(() => {
-    console.log(inView);
-    if (inView) {
-      console.log('다음 페이지 요청 & inView : ', inView);
+    if (inView && hasNextPage) {
+      fetchNextPage();
+      console.log('inView  : ', inView);
+      console.log('data : ', data);
+      console.log('hasNextPage', hasNextPage);
     }
   }, [inView]);
+
+  if (isError) {
+    navigate('/error');
+  }
+
+  // filter가 업데이트 될 경우, 받아왔던 데이터를 filter에 따라 렌더링
+  useEffect(() => {
+    // setPosts(posts.filter((post) => post.shopName.includes(filter)));
+    setPosts(sampleData.filter((post) => post.shopName.includes(filter)));
+  }, [filter]);
 
   const goWritePost = () => {
     navigate('/post-write-intro');
@@ -105,13 +142,6 @@ const PostListPage = () => {
   const getFilter = (text) => {
     setFilter(text);
   };
-
-  // filter가 업데이트 될 경우, 받아왔던 데이터를 filter에 따라 렌더링
-  // 추후 데이터 페칭 코드가 추가되면 동기적으로 작동하도록 aysnc - await 사용 필요
-  // 데이터 페칭 - posts 업데이트 - 필터에 따라 렌더링
-  useEffect(() => {
-    setPosts(data.filter((post) => post.shopName.includes(filter)));
-  }, [filter]);
 
   return (
     <div className="page--layout">
