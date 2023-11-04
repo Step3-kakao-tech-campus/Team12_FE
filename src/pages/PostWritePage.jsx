7092976653ba7aea13a817c89408ddae30f58333
baegyeong <beth0929@naver.com>
2023-11-05 06:44:04 +0900

    [fix]: FormProvider로 변경
    - FormProvider로 자식 컴포넌트에 methods 뿌림 (props drilling 막음)
    - 필수 항목에 작성을 마치고 제출하면 post 요청(msw)

--- src/pages/PostWritePage.jsx
+++ src/pages/PostWritePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useForm, useWatch } from 'react-hook-form';
+import { useForm, FormProvider } from 'react-hook-form';
 import Swal from 'sweetalert2';
 import OtherNav from '../components/atoms/OtherNav';
 import BtnNavigate from '../components/molecules/BtnNavigate';
@@ -8,32 +8,44 @@ import OrderInfo from '../components/templates/OrderInfo';
 import OrderRequest from '../components/templates/OrderRequest';
 import OrderDeadLine from '../components/templates/OrderDeadLine';
 import CircleNavigate from '../components/organisms/CircleNavigate';
+import { STORE, BEVERAGE } from '../constant/postWrite/orderInfo';
+import { DESTINATION } from '../constant/postWrite/orderRequest';
+import { HOUR, MINUTE } from '../constant/postWrite/orderDeadLine';
 import { registerMessage } from '../utils/alert';
+import dateAndTime from '../utils/dateAndTime';
 
 const PostWritePage = () => {
   const navigate = useNavigate();
   const [focus, setFocus] = useState(1);
+  const methods = useForm();
+  const { handleSubmit } = methods;
 
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      tip: 1000,
-    },
+  const inputValue = methods.watch({
+    control: methods.control,
+    name: [STORE, `${BEVERAGE}[0].value`, DESTINATION, HOUR, MINUTE],
   });
 
-  const orderInfoValue = useWatch({
-    control,
-    name: ['store', 'beverage'],
-  });
+  const onSubmit = (data) => {
+    const request = { ...data };
+    request.finishedAt = dateAndTime(data);
 
-  const requestValue = useWatch({
-    control,
-    name: ['destination'],
-  });
+    fetch('/articles/write', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((result) => console.log(result));
+  };
+
+  const handleAlert = (data) => {
+    if (focus === 3 && inputValue.hour && inputValue.minute) {
+      Swal.fire(registerMessage).then((result) => {
+        if (result.isConfirmed) {
+          onSubmit(data);
+        }
+      });
+    }
+  };
 
   const handlePrev = () => {
     if (focus > 1) {
@@ -43,49 +55,46 @@ const PostWritePage = () => {
     }
   };
 
-  const handleAlert = () => {
-    Swal.fire(registerMessage);
-  };
-
   const handleNext = () => {
-    if (focus === 1 && orderInfoValue[0] && orderInfoValue[1]) {
+    if (focus === 1 && inputValue.store && inputValue.beverage[0].value) {
       setFocus((prev) => prev + 1);
     }
-    if (focus === 2 && requestValue[0]) {
+    if (focus === 2 && inputValue.destination) {
       setFocus((prev) => prev + 1);
     }
-    if (focus === 3) handleAlert();
+    if (focus === 3 && inputValue.hour && inputValue.minute) {
+      handleAlert();
+    }
   };
 
-  const onSubmit = (data) => {
-    console.log(data); // api 연결 후 수정 예정
-  };
-
+  // eslint-disable-next-line
   const currentPage = (function (page) {
     if (page === 1) {
-      return <OrderInfo register={register} storeError={!!errors.store} beverageError={!!errors.beverage} />;
+      return <OrderInfo />;
     }
     if (page === 2) {
-      return <OrderRequest register={register} destinationError={!!errors.destination} />;
+      return <OrderRequest />;
     }
     if (page === 3) {
-      return <OrderDeadLine register={register} deadLineError={!!errors.hour || !!errors.minute} />;
+      return <OrderDeadLine />;
     }
   })(focus);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="page--layout flex flex-col justify-between">
-        <div className="flex flex-col justify-start">
-          <OtherNav />
-          <CircleNavigate navigate={focus} />
-          <div className="p-[35px]">{currentPage}</div>
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(handleAlert)}>
+        <div className="page--layout flex flex-col justify-between">
+          <div className="flex flex-col justify-start">
+            <OtherNav />
+            <CircleNavigate navigate={focus} />
+            <div className="p-[35px]">{currentPage}</div>
+          </div>
+          <div className="mb-8">
+            <BtnNavigate handlePrev={handlePrev} handleNext={handleNext} />
+          </div>
         </div>
-        <div className="mb-8">
-          <BtnNavigate handlePrev={handlePrev} handleNext={handleNext} />
-        </div>
-      </div>
-    </form>
+      </form>
+    </FormProvider>
   );
 };
 
