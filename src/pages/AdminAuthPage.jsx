8e79cc846dcc1f57f99e0409e7c9712b4456a9bd
baegyeong <beth0929@naver.com>
2023-11-05 16:44:37 +0900

    [feat]: 관리자 학생증 인증 상세 페이지 useMutation 적용

--- src/pages/AdminAuthPage.jsx
+++ src/pages/AdminAuthPage.jsx
@@ -1,23 +1,44 @@
 import { useEffect, useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { useParams, useNavigate } from 'react-router-dom';
 import OtherNav from '../components/atoms/OtherNav';
 import AuthDetail from '../components/organisms/AuthDetail';
 import Button from '../components/atoms/Button';
 import { REJECT, APPROVE } from '../constant/auth';
+import adminAuth from '../apis/admin';
 
 const AdminAuthPage = () => {
-  //   const { id } = useParams();
+  const { id } = useParams();
+  const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState([]);
   const btnWidth = 'w-[8rem]';
   const btnHeight = 'h-[2.2rem]';
 
   useEffect(() => {
-    fetch('/admin/auth/1')
+    fetch(`/admin/auth/${id}`)
       .then((response) => response.json())
       .then((data) => {
-        const userArray = data.response;
-        setUserInfo(userArray);
+        setUserInfo(data.response);
       });
   }, []);
+
+  const { mutate: handleAuth } = useMutation({
+    mutationFn: adminAuth,
+    onSuccess: () => {
+      navigate('/admin');
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const handleApprove = () => {
+    handleAuth({ user_id: id });
+  };
+
+  const handleReject = () => {
+    navigate('/admin');
+  };
 
   return (
     <div className="page--layout flex flex-col justify-between">
@@ -29,6 +50,7 @@ const AdminAuthPage = () => {
       </div>
       <div className="flex place-content-around p-[35px] mb-[20px]">
         <Button
+          onClick={handleReject}
           width={btnWidth}
           height={btnHeight}
           bgColor="bg-white"
@@ -37,7 +59,7 @@ const AdminAuthPage = () => {
         >
           {REJECT}
         </Button>
-        <Button width={btnWidth} height={btnHeight} bgColor="bg-blue">
+        <Button onClick={handleApprove} width={btnWidth} height={btnHeight} bgColor="bg-blue">
           {APPROVE}
         </Button>
       </div>
