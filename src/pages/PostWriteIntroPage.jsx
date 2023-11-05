e7f71592e2497682311b0f715abe888169c84a87
baegyeong <beth0929@naver.com>
2023-11-05 14:57:44 +0900

    [fix]: 안내 문구 상수 데이터 지정

--- src/pages/PostWriteIntroPage.jsx
+++ src/pages/PostWriteIntroPage.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import Button from '../components/atoms/Button';
 import OtherNav from '../components/atoms/OtherNav';
 import routes from '../constant/routes';
+import intro from '../constant/postWrite/intro';
 
 const PostWriteIntroPage = () => {
   return (
@@ -9,13 +10,13 @@ const PostWriteIntroPage = () => {
       <OtherNav iconColor="#fff" bgColor="#000" />
       <div>
         <div className="text-white p-8">
-          <div className="font-bold text-xl">어떤 음료를 픽업 받으실 건가요?</div>
-          <div>픽업을 위한 정보를 입력합니다.</div>
+          <div className="font-bold text-xl">{intro.TITLE}</div>
+          <div>{intro.SUB_TITLE}</div>
         </div>
         <div className="flex justify-center mb-14">
           <Link to={routes.postWrite}>
             <Button width="w-[19rem]" height=" h-[2.5rem]" bgColor="bg-white" textColor="text-sky-blue">
-              공고 작성하기
+              {intro.START_POST}
             </Button>
           </Link>
         </div>
