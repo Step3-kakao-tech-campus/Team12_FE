6f7022a5386f3efae3ed06453155d302a61ced56
baegyeong <beth0929@naver.com>
2023-11-05 15:27:37 +0900

    [style]: 관리자 학생증 인증 상세 페이지 레이아웃

--- src/pages/AdminAuthPage.jsx
+++ src/pages/AdminAuthPage.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react';
 import OtherNav from '../components/atoms/OtherNav';
 import AuthDetail from '../components/organisms/AuthDetail';
+import Button from '../components/atoms/Button';
+import { REJECT, APPROVE } from '../constant/auth';
 
 const AdminAuthPage = () => {
   //   const { id } = useParams();
   const [userInfo, setUserInfo] = useState([]);
+  const btnWidth = 'w-[8rem]';
+  const btnHeight = 'h-[2.2rem]';
 
   useEffect(() => {
     fetch('/admin/auth/1')
@@ -16,10 +20,26 @@ const AdminAuthPage = () => {
   }, []);
 
   return (
-    <div className="page--layout">
-      <OtherNav />
-      <div className="pt-[25px] p-[35px]">
-        <AuthDetail user={userInfo} />
+    <div className="page--layout flex flex-col justify-between">
+      <div>
+        <OtherNav />
+        <div className="pt-[25px] p-[35px]">
+          <AuthDetail user={userInfo} />
+        </div>
+      </div>
+      <div className="flex place-content-around p-[35px] mb-[20px]">
+        <Button
+          width={btnWidth}
+          height={btnHeight}
+          bgColor="bg-white"
+          textColor="text-blue"
+          bdRadius="rounded-lg border-blue border-2"
+        >
+          {REJECT}
+        </Button>
+        <Button width={btnWidth} height={btnHeight} bgColor="bg-blue">
+          {APPROVE}
+        </Button>
       </div>
     </div>
   );
