77748b2fceee70ab1b8548e34ff90fa449a1a1f2
rktdnjs <rktdnjs@naver.com>
2023-11-07 17:55:40 +0900

    [feat] : 작성한 & 수락한 공고글 페이지 연결 및 추가

--- src/components/templates/mypage/StudentMyPageTemplate.jsx
+++ src/components/templates/mypage/StudentMyPageTemplate.jsx
@@ -1,21 +1,27 @@
 import { CiEdit } from 'react-icons/ci';
 import { PiCaretRightLight, PiReadCvLogoThin, PiCheckSquareLight } from 'react-icons/pi';
+import { Link } from 'react-router-dom';
+import routes from '../../../constant/routes';
 
 const StudentMyPageTemplate = () => {
   return (
     <div>
       <div className="w-full h-1 bg-zinc-200" />
       <div className="px-5 mt-5">
-        <div className="py-3 text-lg">
-          <PiReadCvLogoThin className="mx-2 inline-block" size={25} />
-          <div className="inline-block">작성한 공고글</div>
-          <PiCaretRightLight className="float-right" size={30} />
-        </div>
-        <div className="py-3 text-lg">
-          <PiCheckSquareLight className="mx-2 inline-block" size={25} />
-          <div className="inline-block">수락한 공고글</div>
-          <PiCaretRightLight className="float-right" size={30} />
-        </div>
+        <Link to={routes.writenPost}>
+          <div className="py-3 text-lg">
+            <PiReadCvLogoThin className="mx-2 inline-block" size={25} />
+            <div className="inline-block">작성한 공고글</div>
+            <PiCaretRightLight className="float-right" size={30} />
+          </div>
+        </Link>
+        <Link to={routes.pickupPost}>
+          <div className="py-3 text-lg">
+            <PiCheckSquareLight className="mx-2 inline-block" size={25} />
+            <div className="inline-block">수락한 공고글</div>
+            <PiCaretRightLight className="float-right" size={30} />
+          </div>
+        </Link>
         <div className="py-3 text-lg">
           <CiEdit className="mx-2 inline-block" size={25} />
           <div className="inline-block">개인정보 수정</div>
