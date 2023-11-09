5116e7900722340944667a0f05daf31194e3d619
rktdnjs <rktdnjs@naver.com>
2023-11-09 23:24:47 +0900

    [conf] : Docker 파일 수정

--- src/index.js
+++ src/index.js
@@ -18,13 +18,19 @@ async function deferRender() {
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-deferRender().then(() => {
-  root.render(
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>,
-  );
-});
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
+
+// deferRender().then(() => {
+//   root.render(
+//     <QueryClientProvider client={queryClient}>
+//       <App />
+//     </QueryClientProvider>,
+//   );
+// });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
