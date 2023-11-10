fa3c39f0460ae14652da3616b4c5df4b8462ef4a
rktdnjs <rktdnjs@naver.com>
2023-11-11 04:22:48 +0900

    [refactor] : 마이페이지 작성한 & 수락한 공고글 isLoading 적용

--- src/pages/mypage/MyPagePickupArticleListPage.jsx
+++ src/pages/mypage/MyPagePickupArticleListPage.jsx
@@ -4,12 +4,13 @@ import MyPagePickupArticleCards from '@components/molecules/MyPagePickupArticleC
 import { useQuery } from '@tanstack/react-query';
 import { getPickupArticles } from '@/apis/article';
 import occurError from '@/utils/occurError';
+import Loader from '@/components/atoms/Loader';
 
 const PickupArticlePage = () => {
   // 픽업한 공고글 목록 조회(마이페이지) 요청
   // 데이터를 받아와서 공고글 목록을 articles 변수에 담고
   // MyPagePickupArticleCards로 articles를 props로 넘겨주면 끝
-  const { data: articles } = useQuery(['getPickupArticles'], getPickupArticles, {
+  const { data: articles, isLoading } = useQuery(['getPickupArticles'], getPickupArticles, {
     select: (data) => data?.response?.content,
     onError: (error) => {
       occurError(error);
@@ -27,7 +28,7 @@ const PickupArticlePage = () => {
       <OtherNav />
       <div className="text-center text-xl text-blue my-6">수락한 공고글</div>
       <div className="h-[600px] overflow-y-auto overflow-x-hidden">
-        <MyPagePickupArticleCards articles={articles} />
+        {isLoading ? <Loader /> : <MyPagePickupArticleCards articles={articles} />}
       </div>
     </div>
   );
