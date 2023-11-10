0369d1087b7f61709baa5013bd637b757a117536
baegyeong <beth0929@naver.com>
2023-11-10 21:56:04 +0900

    [fix]: 주석 삭제

--- src/constant/auth.js
+++ src/constant/auth.js
@@ -3,3 +3,8 @@ export const APPROVE = '승인';
 export const LOGIN = `로그인 중이에요!
 잠시만 기다려주세요😊
 `;
+export const INFO = {
+  FIRST_MSG: '이제 편리하게',
+  SECOND_MSG: '음료를 픽업하고, 픽업받아요!',
+  KAKAO: '카카오 로그인',
+};
