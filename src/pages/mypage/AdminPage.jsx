30613580ca738cb94e0fe3603840239fff5453a6
baegyeong <beth0929@naver.com>
2023-11-10 22:05:40 +0900

    [fix]: isLoading, isError 상태 처리

--- src/pages/mypage/AdminPage.jsx
+++ src/pages/mypage/AdminPage.jsx
@@ -1,42 +1,43 @@
 /* eslint-disable */
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import AuthRequest from '@components/organisms/AuthRequest';
 import OtherNav from '@components/atoms/nav/OtherNav';
 import { adminAuthList } from '@/apis/admin';
 import { useInView } from 'react-intersection-observer';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import alertError from '@/constant/alertError';
+import occurError from '@/utils/occurError';
 
 const AdminPage = () => {
-  const [userInfo, setUserInfo] = useState([]);
+  // const [userInfo, setUserInfo] = useState([]);
   const { inView } = useInView();
 
   // msw
-  useEffect(() => {
-    fetch('/admin/auth/list')
-      .then((response) => response.json())
-      .then((data) => {
-        const userArray = data.response.user;
-        setUserInfo(userArray);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch('/admin/auth/list')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const userArray = data.response.user;
+  //       setUserInfo(userArray);
+  //     });
+  // }, []);
 
   // react-query
-  const { data, fetchNextPage, isLoading, refetch } = useInfiniteQuery(
-    ['adminAuthList'],
-    ({ pageParam = 0 }) => adminAuthList(pageParam),
-    {
-      getNextPageParam: (lastPage) => (!lastPage.isLast ? lastPage.nextPage : undefined),
-      onSuccess: (data) => {
-        console.log(data);
-      },
-      onError: (error) => {
-        alert(alertError(error));
-        console.error(error);
-      },
-      retry: false,
+  const {
+    data: userData,
+    fetchNextPage,
+    isLoading,
+    refetch,
+  } = useInfiniteQuery(['adminAuthList'], ({ pageParam = 0 }) => adminAuthList(pageParam), {
+    getNextPageParam: (lastPage) => (!lastPage.isLast ? lastPage.nextPage : undefined),
+    select: (data) => data?.data?.response.content,
+    onSuccess: (data) => {
+      console.log(data);
     },
-  );
+    onError: (error) => {
+      occurError(error);
+    },
+    retry: false,
+  });
 
   useEffect(() => {
     if (inView) {
@@ -44,15 +45,26 @@ const AdminPage = () => {
     }
   }, [inView]);
 
+  const isInData = (userData) => {
+    if (isLoading) {
+      return <Loader />;
+    }
+
+    if (isError) {
+      navigate('/errorPage');
+    }
+
+    return userData.map((item) => {
+      return <AuthRequest key={item.id} user={item} />;
+    });
+  };
+
   return (
     <div className="page--layout">
       <OtherNav />
       <div className="pt-[25px] p-[35px]">
         <div className="text-center text-xl text-blue mb-10">학생 인증 요청</div>
-        {userInfo.map((item) => {
-          return <AuthRequest key={item.id} user={item} />;
-        })}
-        {isLoading && <div>로딩중</div>}
+        {isInData(userData)}
       </div>
     </div>
   );
