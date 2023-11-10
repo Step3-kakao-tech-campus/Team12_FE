411bad22de0897c90f4413ee327e0209670630fe
baegyeong <beth0929@naver.com>
2023-11-11 02:12:43 +0900

    [fix]: 사용하지 않는 prop 삭제

--- src/components/atoms/ErrorMsg.jsx
+++ src/components/atoms/ErrorMsg.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line
 import { ErrorMessage } from '@hookform/error-message';
 
-const ErrorMsg = ({ errors, name, as, render }) => {
-  return <ErrorMessage className="text-red-600 text-sm" errors={errors} name={name} as={as} render={render} />;
+const ErrorMsg = ({ errors, name, as }) => {
+  return <ErrorMessage className="text-red-600 text-sm" errors={errors} name={name} as={as} />;
 };
 
 export default ErrorMsg;
