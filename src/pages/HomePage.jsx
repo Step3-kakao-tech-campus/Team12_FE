c28a3691fc93a3b1d2e8742dd487485859929834
baegyeong <beth0929@naver.com>
2023-11-10 17:56:07 +0900

    [fix]: 최신 공고글 데이터에 대해 에러처리 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,45 +9,52 @@ import Carousel from '@components/atoms/CarouselCustomNavigation';
 import routes from '@/constant/routes';
 import { getLastArticles } from '@/apis/article';
 import Cards from '@/components/molecules/Cards';
+import Loader from '@/components/atoms/Loader';
+import ErrorPage from './ErrorPage';
 
 const HomePage = () => {
-  const { data } = useQuery(['/articles?limit=3'], getLastArticles());
-  const articles = data?.data.response.content;
+  const {
+    data: articles,
+    isLoading,
+    isError,
+  } = useQuery(['/articles?limit=3'], getLastArticles(), {
+    select: (data) => data?.data.response.content,
+  });
+
+  const dark = 'opacity-25';
 
   const isArticles = (articles) => {
-    if (articles.length !== 0) {
+    if (articles) {
       return <Cards articles={articles} />;
     }
-    return;
+
+    if (isLoading) {
+      return (
+        <div className="relative top-[10rem]">
+          <Loader />
+        </div>
+      );
+    }
+
+    if (isError) {
+      return <ErrorPage />;
+    }
   };
 
   return (
     <div className="page--layout">
       <Nav />
-      {/* 사진 or 캐러셀 부분 */}
       <div className="mt-28">
         <Carousel />
       </div>
-      {/* <div className="mt-28">
-        <img
-          src="https://media.istockphoto.com/id/517157936/ko/%EC%82%AC%EC%A7%84/%EA%B5%BF-%EB%AA%A8%EB%8B%9D-%EC%BB%A4%ED%94%BC%EB%A5%BC-%EC%8B%9C%EC%9E%91.jpg?s=612x612&w=0&k=20&c=q9_3X3T3vqxgYi9CQ_fMFa0ZkG77tJN3pQqgzBrYYcE="
-          alt="MainPage-img"
-          width={360}
-        />
-      </div> */}
-      {/* 현재 픽업을 기다리고 있어요 부분 */}
       <div>
         <div className="mt-10 px-6 flex justify-between items-center">
           <div className="text-xl text-blue">현재 픽업을 기다리고 있어요.</div>
-          {/* eslint-disable-next-line */}
           <Link to={routes.article}>
             <div className="text-zinc-400">더보기</div>
           </Link>
         </div>
-        <div className="mx-5">
-          {isArticles(articles)}
-          {/* <Cards articles={articles} /> */}
-        </div>
+        <div className="mx-5">{isArticles(articles)}</div>
       </div>
       <Footer />
     </div>
