d0331f01d2fc6ea6101340846e630bd8b0e1058f
rktdnjs <rktdnjs@naver.com>
2023-11-11 04:26:16 +0900

    [refactor] : 마이페이지 작성한 & 수락한 공고글 상세페이지 isLoading 적용

--- src/pages/mypage/MyPagePickupArticleDetailPage.jsx
+++ src/pages/mypage/MyPagePickupArticleDetailPage.jsx
@@ -4,29 +4,25 @@ import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import { getMyPagePickupArticleDetail } from '@/apis/articleDetail';
 import occurError from '@/utils/occurError';
+import Loader from '@/components/atoms/Loader';
 
 const MyPagePickupArticleDetailPage = () => {
   // useParams + useQuery로 데이터를 받아와서
   // data를 data?.response로 받아서 PickerMatchTemplate으로 보내주면 됨
   const { id } = useParams();
-  const { data: articles } = useQuery(['getMyPagePickupArticleDetail'], () => getMyPagePickupArticleDetail(id), {
+  const { data: articles, isLoading } = useQuery(['getMyPagePickupArticleDetail'], getMyPagePickupArticleDetail(id), {
     select: (data) => data?.response?.content,
     onError: (error) => {
       occurError(error);
     },
   });
 
-  console.log(articles);
   // useQuery data 디버깅용
   useEffect(() => {
     console.log(articles);
   }, [articles]);
 
-  return (
-    <div className="page--layout">
-      <PickerMatchTemplate response={articles} />
-    </div>
-  );
+  return <div className="page--layout">{isLoading ? <Loader /> : <PickerMatchTemplate response={articles} />}</div>;
 };
 
 export default MyPagePickupArticleDetailPage;
