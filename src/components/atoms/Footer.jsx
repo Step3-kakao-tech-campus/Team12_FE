af0644a45db2621f12c0a18646c1329cbafc2d9c
baegyeong <beth0929@naver.com>
2023-11-11 18:49:45 +0900

    [style]: footer 스타일 변경

--- src/components/atoms/Footer.jsx
+++ src/components/atoms/Footer.jsx
@@ -2,12 +2,12 @@ import { HOME, FOOTER } from '@/constant/home';
 
 const Footer = () => {
   return (
-    <div className="bg-zinc-100 p-5 w-full absolute bottom-0">
-      <span className="text-zinc-400">{HOME.PICKUP_SHUTTLE}</span>
+    <div className="bg-zinc-100 py-2 px-5 w-full absolute bottom-0">
+      <span className="text-zinc-400 text-sm">{HOME.PICKUP_SHUTTLE}</span>
       <br />
       <span className="text-zinc-400 text-xs">{FOOTER.TEAM}</span>
       <hr className="border-1 border-zinc-300" />
-      <div className="text-right text-sm m-1 text-zinc-400">{FOOTER.COPYRIGHT} &#169;</div>
+      <div className="text-right text-xs m-1 text-zinc-400">{FOOTER.COPYRIGHT} &#169;</div>
     </div>
   );
 };
