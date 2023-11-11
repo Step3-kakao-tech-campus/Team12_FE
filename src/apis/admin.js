85a04588b19e82a6155354a84442af1a35d2fec4
rktdnjs <rktdnjs@naver.com>
2023-11-11 11:50:31 +0900

    [fix] : 경로가 잘못 요청되는 버그, instance를 사용하지 않고 바로 axios사용하는 형태로 수정

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -1,17 +1,44 @@
-import { instance } from './index';
+import axios from 'axios';
 
+const axiosConfig = {
+  timeout: 1000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+};
+
+// 크램폴린 환경에서 경로가 잘못 설정되어 요청되어 다음 로직으로 바꿔서 적용
 export const adminAuth = () => {
-  return instance.put('/admin/auth/approval');
+  return axios.put('/admin/auth/approval', {}, axiosConfig);
 };
 
 export const adminAuthReject = () => {
-  return instance.put('admin/auth/reject');
+  return axios.put('/admin/auth/reject', {}, axiosConfig);
 };
 
 export const adminAuthDetail = (id = 1) => {
-  return instance.get(`/admin/auth/list/${id}`);
+  return axios.get(`/admin/auth/list/${id}`, axiosConfig);
 };
 
 export const adminAuthList = (offset = '') => {
-  return instance.get(`/admin/auth/list?offset=${offset}&limit=10`);
+  return axios.get(`/admin/auth/list?offset=${offset}&limit=10`, axiosConfig);
 };
+
+// import { instance } from './index';
+
+// export const adminAuth = () => {
+//   return instance.put('/admin/auth/approval');
+// };
+
+// export const adminAuthReject = () => {
+//   return instance.put('admin/auth/reject');
+// };
+
+// export const adminAuthDetail = (id = 1) => {
+//   return instance.get(`/admin/auth/list/${id}`);
+// };
+
+// export const adminAuthList = (offset = '') => {
+//   return instance.get(`/admin/auth/list?offset=${offset}&limit=10`);
+// };
