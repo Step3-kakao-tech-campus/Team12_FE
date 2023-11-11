c855d81a730aeb89ff23d1c5b07ca152befe149c
baegyeong <beth0929@naver.com>
2023-11-11 14:45:27 +0900

    [fix]: info 컴포넌트를 pickupinfo 컴포넌트로 변경

---
+++ src/components/organisms/PickUpInfo.jsx
@@ -0,0 +1,68 @@
+import ArticleInfo from '@components/organisms/ArticleInfo';
+import { time } from '@/utils/date';
+import { ARTICLE, PICKUP } from '@/constant/article';
+
+const PickUpInfo = ({
+  isMatch,
+  finishedAt,
+  beverages,
+  tip,
+  request,
+  pickerBank,
+  pickerAccount,
+  arrivalTime,
+  pickerPhoneNumber,
+}) => {
+  const TITLE = 'text-xl font-bold text-blue py-1';
+  const TEXT_GRAY = 'text-zinc-400';
+
+  return (
+    <div className="px-8 pt-6">
+      <article>
+        <p className={TITLE}>{ARTICLE.PICKUPINFO}</p>
+        <div>
+          {beverages.map((name) => {
+            return <p key={name.id}>{name}</p>;
+          })}
+        </div>
+      </article>
+
+      <article className="my-12">
+        <p className={TITLE}>{ARTICLE.REQUEST}</p>
+        <section>
+          <div className="flex">
+            <p className={TEXT_GRAY}>{ARTICLE.TIP}</p>
+            <p className="ml-8">
+              {tip}
+              {ARTICLE.WON}
+            </p>
+          </div>
+          <div className="flex">
+            <p className={TEXT_GRAY}>{ARTICLE.REQUEST}</p>
+            <p className="ml-5">{request}</p>
+          </div>
+        </section>
+      </article>
+
+      <article className="my-12">
+        <p className={TITLE}>{ARTICLE.DEADLINE}</p>
+        <p>{time(finishedAt)}</p>
+      </article>
+
+      {isMatch && (
+        <article className="my-8 px-8">
+          <div className="text-xl font-bold text-blue pb-2">{PICKUP.INFO}</div>
+          <ArticleInfo label={PICKUP.ARRIVAL_TIME}>{time(arrivalTime)}</ArticleInfo>
+          <ArticleInfo className="my-1" label={PICKUP.ACCOUNT}>
+            {pickerBank} {pickerAccount}
+          </ArticleInfo>
+          <ArticleInfo className="my-1" label={PICKUP.PHONE_NUMBER}>
+            {pickerPhoneNumber}
+          </ArticleInfo>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default PickUpInfo;
