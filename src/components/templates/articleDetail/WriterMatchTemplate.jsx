5a668b533a709cd7b7cc5047505e64114ceb5323
baegyeong <beth0929@naver.com>
2023-11-11 14:51:25 +0900

    [fix]: 작성자일 때 매치 여부 보여주는 컴포넌트 수정

--- src/components/templates/articleDetail/WriterMatchTemplate.jsx
+++ src/components/templates/articleDetail/WriterMatchTemplate.jsx
@@ -1,34 +1,45 @@
 import OtherNav from '@components/atoms/nav/OtherNav';
-import Info from '@components/atoms/Info';
-import Location from '@components/organisms/Location';
-import WriterMatch from '@components/organisms/WriterMatch';
-import Time from '@/utils/time';
+import PickUpInfo from '@components/organisms/PickUpInfo';
+import LocationInfo from '@components/organisms/LocationInfo';
 import { PICKUP } from '@/constant/article';
 
 const WriterMatchTemplate = ({ response }) => {
+  const {
+    shopName,
+    destination,
+    finishedAt,
+    beverages,
+    tip,
+    request,
+    pickerBank,
+    pickerAccount,
+    arrivalTime,
+    pickerPhoneNumber,
+  } = response;
+
   return (
     <>
-      {/* 파란색 부분 */}
       <div className="bg-sky-blue h-64 rounded-b-3xl">
         <OtherNav iconColor="#fff" bgColor="#000" />
+
         <div className="px-[25px] mt-1 text-white text-xl">
           <p className="block">{PICKUP.LABEL}</p>
           <p>{PICKUP.SUB_LABEL}</p>
-          <Location response={response} />
+          <LocationInfo shopName={shopName} destination={destination} />
         </div>
       </div>
-      {/* 하얀색 부분 */}
-      <Info response={response} />
-      <article className="my-8 px-8">
-        <div className="text-xl font-bold text-blue pb-2">{PICKUP.INFO}</div>
-        <WriterMatch label={PICKUP.ARRIVAL_TIME}>{Time(response.arrivalTime)}</WriterMatch>
-        <WriterMatch className="my-1" label={PICKUP.ACCOUNT}>
-          {response.pickerBank} {response.pickerAccount}
-        </WriterMatch>
-        <WriterMatch className="my-1" label={PICKUP.PHONE_NUMBER}>
-          {response.pickerPhoneNumber}
-        </WriterMatch>
-      </article>
+
+      <PickUpInfo
+        isMatch="true"
+        finishedAt={finishedAt}
+        beverages={beverages}
+        tip={tip}
+        request={request}
+        pickerBank={pickerBank}
+        pickerAccount={pickerAccount}
+        arrivalTime={arrivalTime}
+        pickerPhoneNumber={pickerPhoneNumber}
+      />
     </>
   );
 };
--- src/components/templates/articleDetail/WriterNoMatchTemplate.jsx
+++ src/components/templates/articleDetail/WriterNoMatchTemplate.jsx
@@ -1,10 +1,9 @@
-/* eslint-disable */
 import { BsThreeDotsVertical } from 'react-icons/bs';
 import Swal from 'sweetalert2';
 import { useNavigate, useParams } from 'react-router-dom';
 import OtherNav from '@components/atoms/nav/OtherNav';
-import Info from '@components/atoms/Info';
-import Location from '@components/organisms/Location';
+import PickUpInfo from '@components/organisms/PickUpInfo';
+import LocationInfo from '@components/organisms/LocationInfo';
 import { useMutation } from '@tanstack/react-query';
 import { controlArticleMessage, deleteArticleMessage, articleDeleteSuccessMessage } from '@/utils/alert';
 import routes from '@/constant/routes';
@@ -13,27 +12,29 @@ import { MATCHING_STATE } from '@/constant/article';
 import occurError from '@/utils/occurError';
 
 const WriterNoMatchTemplate = ({ response }) => {
+  const { shopName, destination, finishedAt, beverages, tip, request } = response;
   const { id } = useParams();
   const navigate = useNavigate();
-  const deleteMutation = useMutation({
+
+  const { mutate: deleteMutation } = useMutation({
     mutationFn: deleteArticle,
+    onSuccess: () => {
+      Swal.fire(articleDeleteSuccessMessage).then(navigate(routes.article));
+    },
     onError: (error) => {
       occurError(error);
     },
   });
 
-  // 삭제 버튼 눌렀을 때
   const articleDelete = () => {
     Swal.fire(deleteArticleMessage).then((result) => {
       if (result.isConfirmed) {
-        deleteMutation.mutate(id);
-        Swal.fire(articleDeleteSuccessMessage).then(navigate(routes.article));
+        deleteMutation({ id });
       }
     });
   };
 
-  // 우상단 점3개 눌렀을 때
-  const articleControl = () => {
+  const handleOnOption = () => {
     return Swal.fire(controlArticleMessage).then((result) => {
       if (result.isConfirmed) {
         navigate(routes.articleWrite);
@@ -45,22 +46,21 @@ const WriterNoMatchTemplate = ({ response }) => {
 
   return (
     <>
-      {/* 파란색 부분 */}
       <div className="bg-sky-blue h-60 rounded-b-3xl">
         <OtherNav iconColor="#fff" bgColor="#000" />
+
         <div className="px-[25px]">
           <div className="flex justify-between items-center">
             <div className="text-white text-xl">{MATCHING_STATE.WAITING_MATCHING}</div>
-            {/* eslint-disable-next-line */}
-            <button onClick={articleControl}>
+            <button onClick={handleOnOption}>
               <BsThreeDotsVertical size="25" style={{ color: 'white' }} />
             </button>
           </div>
-          <Location response={response} />
+          <LocationInfo shopName={shopName} destination={destination} />
         </div>
       </div>
-      {/* 하얀색 부분 */}
-      <Info response={response} />
+
+      <PickUpInfo isMatch="false" finishedAt={finishedAt} beverages={beverages} tip={tip} request={request} />
     </>
   );
 };
