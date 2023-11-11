87473ac3a84a7de95ee8af41a6415f86f31c8819
rktdnjs <rktdnjs@naver.com>
2023-11-11 12:03:26 +0900

    [style] : 관리자 페이지 개인정보 수정 탭 CSS 수정

--- src/components/templates/mypage/AdminMyPageTemplate.jsx
+++ src/components/templates/mypage/AdminMyPageTemplate.jsx
@@ -21,7 +21,7 @@ const AdminMyPageTemplate = () => {
         <MyPageMenu to={routes.pickupArticle} icon={<PiCheckSquareLight className={iconStyle} size={25} />}>
           {mypage.APPROVAL_ARTICLE}
         </MyPageMenu>
-        <MyPageMenu icon={<CiEdit className={iconStyle} />}>{mypage.EDIT_INFO}</MyPageMenu>
+        <MyPageMenu icon={<CiEdit className={iconStyle} size={25} />}>{mypage.EDIT_INFO}</MyPageMenu>
       </div>
     </div>
   );
