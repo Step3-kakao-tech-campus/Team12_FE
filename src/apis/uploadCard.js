7fa378bd87478dadb484d71dba5119cbb7e0fdb1
rktdnjs <rktdnjs@naver.com>
2023-11-13 22:39:39 +0900

    [fix] : 학생증 업로드 API 수정

--- src/apis/uploadCard.js
+++ src/apis/uploadCard.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const imageInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'multipart/form-data',
   },
