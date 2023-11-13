d014c8d3a561499b2a76aa3737ef0cc98d9ca582
rktdnjs <rktdnjs@naver.com>
2023-11-13 22:21:57 +0900

    [fix] : error 메시지 폰트가 깨지는 부분 수정

--- src/constant/error.js
+++ src/constant/error.js
@@ -9,8 +9,8 @@ export const ERROR = {
   입력하신 페이지의 주소가 정확한지 다시 한 번 확인해 주세요!
   `,
   GO_HOME: '픽업셔틀 홈으로 돌아가기',
-  NO_APPROVAL_ARTICLE: '수락한 공고글이 없습니다.🥲',
-  NO_WRITTEN_ARTICLE: '작성한 공고글이 없습니다.🥲',
-  NO_USER_LIST: '요청한 유저 인증이 없습니다.🥲',
-  NO_ARTICLE_INFO: '해당 공고의 데이터가 없습니다.🥲',
+  NO_APPROVAL_ARTICLE: '수락한 공고글이 없습니다.',
+  NO_WRITTEN_ARTICLE: '작성한 공고글이 없습니다.',
+  NO_USER_LIST: '요청한 유저 인증이 없습니다.',
+  NO_ARTICLE_INFO: '해당 공고의 데이터가 없습니다.',
 };
