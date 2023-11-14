e3b7d24ba29f8793ee529c6c3847988481858cd4
rktdnjs <rktdnjs@naver.com>
2023-11-14 14:02:52 +0900

    [refactor] : 관리자 페이지 학생증 인증 페이지 삭제

--- src/components/templates/mypage/AdminMyPageTemplate.jsx
+++ src/components/templates/mypage/AdminMyPageTemplate.jsx
@@ -1,10 +1,9 @@
 import { CiEdit } from 'react-icons/ci';
-import { PiReadCvLogoThin, PiCheckSquareLight, PiGraduationCapLight } from 'react-icons/pi';
+import { PiReadCvLogoThin, PiCheckSquareLight } from 'react-icons/pi';
 import { RiAdminLine } from 'react-icons/ri';
 import MyPageMenu from '@components/organisms/MyPageMenu';
 import MYPAGE from '@/constant/mypage';
 import routes from '@/constant/routes';
-import { STUDENT } from '@/constant/auth';
 
 const AdminMyPageTemplate = () => {
   const iconStyle = 'mx-2 inline-block';
@@ -24,10 +23,6 @@ const AdminMyPageTemplate = () => {
         </MyPageMenu>
         <MyPageMenu to={routes.mypageModify} icon={<CiEdit className={iconStyle} size={25} />}>
           {MYPAGE.EDIT_INFO}
-        </MyPageMenu>
-        {/* 임시 학생증인증 */}
-        <MyPageMenu to={routes.uploadStudentCard} icon={<PiGraduationCapLight className={iconStyle} size={25} />}>
-          {STUDENT.TITLE}
         </MyPageMenu>
       </div>
     </div>
